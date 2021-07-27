13e43edef3389ecc6507207d05d4edddcb0186e2
ayumitanaka <tanakayumi13@gmail.com>
2021-07-27 16:39:12 +0000

    solved rocket-ship

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,11 +1,12 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component, memo } from 'react';
+import _isEqual from 'lodash.isequal';
 import RocketCore from './RocketCore';
 
-export function FunctionalRocket() {
+export const FunctionalRocket = memo(() => {
   const [initialLaunchTime] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
-}
+})
 
 export class ClassRocket extends Component {
   constructor() {
@@ -14,6 +15,10 @@ export class ClassRocket extends Component {
     this.state = {
       initialLaunchTime: Date.now()
     };
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return !_isEqual(nextProps, this.props);
   }
 
   render() {
